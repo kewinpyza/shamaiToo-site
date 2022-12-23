c734631639956d26c45cf58f1c8d47468c0f7e88
kewinpyza <kewin.pyza@gmail.com>
2022-12-23 20:39:54 +0100

    Add script smooth scrolling

---
+++ script/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// Navigation on mobile app
+const headerNav = document.querySelector("#header-nav");
+const hamburgerMenu = document.querySelector(".btn-hamburger");
+
+hamburgerMenu.addEventListener("click", () => {
+  headerNav.classList.toggle("open-nav");
+});
+
+// Smooth scrooling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    // Scroll to sections
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navi
+    if (link.classList.contains("nav-link")) {
+      headerNav.classList.toggle("nav-open");
+    }
+  });
+});
+
+// Make navigation sticky
+const heroSection = document.querySelector("#section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    // console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Fixing flexbox gap property which is missing in some Safaris
+function checkFlexGap() {
+  const flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  const isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  // console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
