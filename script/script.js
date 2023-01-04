415a2a833daafb54a14cd0bfeedb2f45c525e9ad
kewinpyza <kewin.pyza@gmail.com>
2023-01-04 01:15:23 +0100

    Add section scrolling effect

--- script/script.js
+++ script/script.js
@@ -276,6 +276,28 @@ const obs = new IntersectionObserver(
 );
 obs.observe(heroSection);
 
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.07,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
+
 // Fixing flexbox gap property which is missing in some Safaris
 function checkFlexGap() {
   const flex = document.createElement("div");
